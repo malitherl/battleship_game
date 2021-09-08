e95f4b98f7e6e5a5b3faf599b29d4d682450d858
Michaela <malitherland@gmail.com>
2021-09-08 14:25:47 -0400

    finished creating a gameboard

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function ship (length){
           if(index >= this.length){
               throw Error; 
           } else {
-            this.status[index] = 'Hit';  
+            this.status[index] = 'Hit'; 
+            return this.status[index];
           }
         },
         isSunk(){
@@ -57,10 +58,47 @@ function placement(col, row, ship){
 function gameBoard(){
     let board = new Array(10)
     for(let i =0; i<board.length;i++){
-        board[i] = new Array(10)
+        board[i] = new Array(10);
+        board[i].fill(0, 0)
     }
-    return {
+    let misses =[];
+    return { 
         givenBoard: board,
+        missed: misses,
+        place(ship) {    
+            let coordinatesNeeded = ship.length;
+            let x =0;
+            let y= 0;
+            while(coordinatesNeeded >0){     
+                this.givenBoard[x][y]= ship.type;
+                coordinatesNeeded--; 
+                y++;
+            }
+            board= this.givenBoard;
+            return this.givenBoard;
+        },
+        receiveAttack(x,y, ship0){
+            if (this.givenBoard[x][y] !== 0){
+                this.givenBoard[x][y] = ship0.hit(x);
+                return ship0.hit(x);
+            } else {
+                misses.push("(" + x + ", " + y + ")");
+                return misses;
+            }           
+        },
+        allShipsSunk(){
+            let shipsSunk = true;
+            for(let i= 0; i< 10; i++){
+                for(let j=0; j< 10; j++){
+                    if(this.givenBoard[i][j] !== 0 && this.givenBoard[i][j] !== 'Hit'){
+                        shipsSunk= false;
+                    }
+                }
+            }
+            return shipsSunk;
+        }
+
+
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -10,7 +10,55 @@ test('Coordinates are too small for ship', () => {
     expect(placement([1], ['B','C','D','E'], x)).toEqual('invalid');
 })
 
-test('Board is a 10 x 10 Array', ()=> {
+test('Board is a 10 x 10 Array', ()=> { 
     expect(gameBoard().givenBoard.length).toEqual(10)
     expect(gameBoard().givenBoard[0].length).toEqual(10)
 })
+
+test('Small Destroyer was placed onto Board', ()=>{
+    expect(gameBoard().place(ship(2))).toEqual([
+        ["Destroyer", "Destroyer", 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+})
+
+test('Small Destroyer was hit', ()=>{
+    let game= gameBoard();
+    let ship0 = ship(2)
+    game.place(ship0)
+    expect(game.receiveAttack(0,0, ship0)).toEqual('Hit');
+})
+
+test('Small Destroyer was not hit', ()=>{
+    let game1 = gameBoard();
+    let ship1 = ship(2);
+    game1.place(ship1);
+    game1.receiveAttack(3,3, ship1);
+    game1.receiveAttack(3,4, ship1);
+    game1.receiveAttack(3,5, ship1);
+    expect(game1.missed).toEqual(["(3, 3)", "(3, 4)", "(3, 5)"])
+})
+
+test('Not all Ships have been sunk', ()=>{
+    let game2 = gameBoard();
+    let ship2=ship(5);
+    game2.place(ship2);
+    expect(game2.allShipsSunk()).toEqual(false);
+})
+test('All ships have been sunk', ()=>{
+    let game3 = gameBoard();
+    let ship3= ship(2);
+    game3.place(ship3);
+    game3.receiveAttack(0,0, ship3);
+    game3.receiveAttack(0,1, ship3);
+    expect(game3.allShipsSunk()).toEqual(true);
+})
+
